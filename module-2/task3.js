1794a8a56a55607f98e9df5ba137267a26630109
WerHawer <werhawer@gmail.com>
2019-10-08 23:16:34 +0300

    did task3

---
+++ module-2/task3.js
@@ -0,0 +1,17 @@
+'use strict';
+alert(`Выполняется задание №3 поиск самого длинного слова. Результат в консоли`);
+const findLongestWord = function(str) {
+    const arrStr = str.split(' ');
+    let longestWord = arrStr[0];
+    for (let word of arrStr) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+    return longestWord;
+};
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
